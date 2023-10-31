f68e7019ebd7f1f5a8092160cb0ded3685c94b3c
Tony0101010101 <149443286+Tony0101010101@users.noreply.github.com>
2023-10-31 01:39:00 +0100

    Update DAO.java

--- olimpiadasaqp/src/main/java/modelo/DAO.java
+++ olimpiadasaqp/src/main/java/modelo/DAO.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
-
+/*prueba*/
 /*Edicion*/
 public class DAO {
 	public Connection crearCnx() {
